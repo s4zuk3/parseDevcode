5bef05ba02902429f6e48096bc6ad8ca02bab7f8
Francisco López Alarcón <francisco.lopez.a@usach.cl>
2016-08-01 04:19:42 -0400

    s

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ angular.module('MyApp', ['ngMaterial','ngAnimate', 'ui.bootstrap'])
   .controller('AppCtrl', function($scope,$http) {
 
   $scope.cursos = {};
-  $http.get("ojala.min.json")
+  $http.get("devcode.min.json")
     .then(function(response) {
         $scope.cursos = response.data;
     });
